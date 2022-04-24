73c8744e8eca6404ad06996a4acac6d6eda46988
Clément BRESSY <clement.bressy@protonmail.com>
2022-04-24 22:38:56 +0200

    This is working.

--- arduino-nano_7-segments-dht11/arduino-nano_7-segments-dht11.ino
+++ arduino-nano_7-segments-dht11/arduino-nano_7-segments-dht11.ino
@@ -39,8 +39,8 @@ void update_counter_value();
 const unsigned int DHT11_PIN = 16; // For dht.h
 const unsigned int DHT11_LED_PIN = 3; // For dht11 led on ticker
 
-Ticker dht11_updater(update_dht11_values, 0, 9000);  //calls function update_dht11_values() every 2 seconds, internal resolution is micros, running endless
-Ticker counter_updater(update_counter_value, 0, 1000);  //calls function update_dht11_values() every 2 seconds, internal resolution is micros, running endless
+Ticker dht11_updater(update_dht11_values, 3000);  //calls function update_dht11_values() every 2 seconds, internal resolution is micros, running endless
+Ticker counter_updater(update_counter_value, 1000);  //calls function update_dht11_values() every 2 seconds, internal resolution is micros, running endless
 dht DHT;
 float temp = 0;
 
@@ -63,14 +63,16 @@ void update_counter_value(){
   if (counter > 99) {
     counter = 0;
   }
+  Serial.println(counter);
 }
 
 
 void update_dht11_values() {
-  update_counter_value();
+  //update_counter_value();
+  
   //int chk = DHT.read11(DHT11_PIN);
   digitalWrite(DHT11_LED_PIN, HIGH);
-  delay(500);
+  delay(7);
   digitalWrite(DHT11_LED_PIN, LOW);
   return;
 }
@@ -78,6 +80,9 @@ void update_dht11_values() {
 
 // the setup function runs once when you press reset or power the board
 void setup() {
+
+  Serial.begin(9600);
+  
   // initialize digital pins as an output.
   for (int i = 0; i < 8; i++) {
     pinMode(SEGMENT_PINS[i], OUTPUT);
@@ -87,28 +92,28 @@ void setup() {
   
   pinMode(DHT11_PIN, INPUT);
   pinMode(DHT11_LED_PIN, OUTPUT);
+  
+  delay(1111);
+  
   dht11_updater.start();
   counter_updater.start();
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  // loop every 1 second approx.
-  // 1 second = 1,000,000 microseconds
-  // 1 second = delay(1000)
-  dht11_updater.update();
+  
+  dht11_updater.update();  // It will check the Ticker, and if necessary, will run the callback.
   counter_updater.update();
 
 
-  for (int i = 0; i < 1000 / DELAY / 2; i++) { // loops 10 times per second
+//  for (int i = 0; i < 1000 / DELAY / 2; i++) { // loops 10 times per second
     displayDigit(counter / 10);
     digitalWrite(MULTIPLEXING_PIN, HIGH);
     delay(DELAY);
     displayDigit(counter % 10);
     digitalWrite(MULTIPLEXING_PIN, LOW);
     delay(DELAY);
-  }
+//  }
 
-  //delay(1000);
 
 }
