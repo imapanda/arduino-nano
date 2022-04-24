74e0c08ca2c11fee6f3f4e772a3d7792a7be5400
Clément BRESSY <clement.bressy@protonmail.com>
2022-04-24 19:59:33 +0200

    Not working loop counter

--- arduino-nano_7-segments-display/arduino-nano_7-segments-display.ino
+++ arduino-nano_7-segments-display/arduino-nano_7-segments-display.ino
@@ -14,6 +14,7 @@ const unsigned int MULTIPLEXING_PIN = 5;
 // GPIO Pinout for 7 segment display
 const unsigned int SEGMENT_PINS[] = {12, 11, 10, 9, 8, 7, 6, 5 };   // { a b c d e f g . )
 
+unsigned int counter = 0;
 
 const byte segCode[11][8] = {
   //  a  b  c  d  e  f  g  .
@@ -38,107 +39,30 @@ void displayDigit(int digit)
 }
 
 
-
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pins as an output.
   for (int i = 0; i < 8; i++) {
     pinMode(SEGMENT_PINS[i], OUTPUT);
   }
-
-
+  
   pinMode(MULTIPLEXING_PIN, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-
-  for (int n = 0; n < 10; n++) {    // display digits 0 - 9 and decimal point
-    displayDigit(n);
-    for (int i = 0; i < 200; i++) {
-      digitalWrite(MULTIPLEXING_PIN, HIGH);
-      delay(1);
-      digitalWrite(MULTIPLEXING_PIN, LOW);
-      delay(1);
-    }
+  //loop every 1 second approx.
+  // 1 second = 1,000,000 microseconds
+  for (int i = 0; i < 10; i++) { // loops 10 times per second
+    displayDigit(counter/10);
+    digitalWrite(MULTIPLEXING_PIN, HIGH);
+    delayMicroseconds(100000);
+    displayDigit(counter%10);
+    digitalWrite(MULTIPLEXING_PIN, LOW);
+    delayMicroseconds(100000);
   }
 
-
-
-  digitalWrite(MULTIPLEXING_PIN, HIGH);
-  //  digitalWrite(SEGMENT_A, HIGH);   // turn the LED on (HIGH is the voltage level)
-  //  delay(DELAY);
-  //  digitalWrite(SEGMENT_A, LOW);
-  //  delay(DELAY);
-  //
-  //  digitalWrite(SEGMENT_B, HIGH);   // turn the LED on (HIGH is the voltage level)
-  //  delay(DELAY);
-  //  digitalWrite(SEGMENT_B, LOW);
-  //  delay(DELAY);
-  //
-  //  digitalWrite(SEGMENT_C, HIGH);   // turn the LED on (HIGH is the voltage level)
-  //  delay(DELAY);
-  //  digitalWrite(SEGMENT_C, LOW);
-  //  delay(DELAY);
-  //
-  //  digitalWrite(SEGMENT_D, HIGH);   // turn the LED on (HIGH is the voltage level)
-  //  delay(DELAY);
-  //  digitalWrite(SEGMENT_D, LOW);
-  //  delay(DELAY);
-  //
-  //  digitalWrite(SEGMENT_E, HIGH);   // turn the LED on (HIGH is the voltage level)
-  //  delay(DELAY);
-  //  digitalWrite(SEGMENT_E, LOW);
-  //  delay(DELAY);
-  //
-  //  digitalWrite(SEGMENT_F, HIGH);   // turn the LED on (HIGH is the voltage level)
-  //  delay(DELAY);
-  //  digitalWrite(SEGMENT_F, LOW);
-  //  delay(DELAY);
-  //
-  //  digitalWrite(SEGMENT_G, HIGH);   // turn the LED on (HIGH is the voltage level)
-  //  delay(DELAY);
-  //  digitalWrite(SEGMENT_G, LOW);
-  //  delay(DELAY);
-  //
-  //  digitalWrite(MULTIPLEXING_PIN, LOW);
-  //  digitalWrite(SEGMENT_A, HIGH);   // turn the LED on (HIGH is the voltage level)
-  //  delay(DELAY);
-  //  digitalWrite(SEGMENT_A, LOW);
-  //  delay(DELAY);
-  //
-  //  digitalWrite(SEGMENT_B, HIGH);   // turn the LED on (HIGH is the voltage level)
-  //  delay(DELAY);
-  //  digitalWrite(SEGMENT_B, LOW);
-  //  delay(DELAY);
-  //
-  //  digitalWrite(SEGMENT_C, HIGH);   // turn the LED on (HIGH is the voltage level)
-  //  delay(DELAY);
-  //  digitalWrite(SEGMENT_C, LOW);
-  //  delay(DELAY);
-  //
-  //  digitalWrite(SEGMENT_D, HIGH);   // turn the LED on (HIGH is the voltage level)
-  //  delay(DELAY);
-  //  digitalWrite(SEGMENT_D, LOW);
-  //  delay(DELAY);
-  //
-  //  digitalWrite(SEGMENT_E, HIGH);   // turn the LED on (HIGH is the voltage level)
-  //  delay(DELAY);
-  //  digitalWrite(SEGMENT_E, LOW);
-  //  delay(DELAY);
-  //
-  //  digitalWrite(SEGMENT_F, HIGH);   // turn the LED on (HIGH is the voltage level)
-  //  delay(DELAY);
-  //  digitalWrite(SEGMENT_F, LOW);
-  //  delay(DELAY);
-  //
-  //  digitalWrite(SEGMENT_G, HIGH);   // turn the LED on (HIGH is the voltage level)
-  //  delay(DELAY);
-  //  digitalWrite(SEGMENT_G, LOW);
-  //  delay(DELAY);
-
-  //  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  //  delay(100);                       // wait for a second
-  //  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  //  delay(100);                       // wait for a second
+  
+  counter++;
+  if(counter > 99){counter=0;}
 }
