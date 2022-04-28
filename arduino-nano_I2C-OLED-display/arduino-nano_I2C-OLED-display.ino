03734543103bb8f9f37b2925354b975ea3b2134d
Clément BRESSY <clement.bressy@protonmail.com>
2022-04-29 00:20:31 +0200

    Making it work !

--- arduino-nano_I2C-OLED-display/arduino-nano_I2C-OLED-display.ino
+++ arduino-nano_I2C-OLED-display/arduino-nano_I2C-OLED-display.ino
@@ -17,9 +17,11 @@
 #define SCREEN_HEIGHT        32
 #define SCREEN_RESET_PIN     -1 // -1 if your screen doesn't have reset pin
 #define SCREEN_I2C_ADDRESS 0x3C
+//#define SCREEN_I2C_ADDRESS 0x3D
 
 // &Wire => includes library « Wire » in instanciation, indicates we are working on « I2C » OLED 
 Adafruit_SSD1306 ecranOLED(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, SCREEN_RESET_PIN);
+Adafruit_SSD1306 ecranOLED_2(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, SCREEN_RESET_PIN);
 
 // 3 colors available in Adafruit_SSD1306.h
 #define SSD1306_BLACK 0             // Draw 'off' pixels
@@ -32,16 +34,36 @@ void setup() {
     // Init OLED screen
   if(!ecranOLED.begin(SSD1306_SWITCHCAPVCC, SCREEN_I2C_ADDRESS))
     while(1);                               // Infinite loop if can't init screen
+  // Init OLED screen 2
+  //if(!ecranOLED_2.begin(SSD1306_SWITCHCAPVCC, 0x3D))
+  //  while(1);                               // Infinite loop if can't init screen
 
   ecranOLED.clearDisplay();
-  ecranOLED.setTextSize(3);  // Caracters size (1 to 3)
+  ecranOLED.display();
+  
+  ecranOLED_2.clearDisplay();
+  ecranOLED_2.display();
+
+  ecranOLED.clearDisplay();
+  ecranOLED.setTextSize(2);  // Caracters size (1 to 3)
   ecranOLED.setCursor(0, 0);
 
   ecranOLED.setTextColor(SSD1306_WHITE);
 
-  ecranOLED.println("TEST");
+  ecranOLED.println("ecranOLED");
 
   ecranOLED.display();                            // Transfer buffer to screen
+  delay(2000);
+  
+  ecranOLED_2.clearDisplay();
+  ecranOLED_2.setTextSize(1);  // Caracters size (1 to 3)
+  ecranOLED_2.setCursor(0, 0);
+
+  ecranOLED_2.setTextColor(SSD1306_WHITE);
+
+  ecranOLED_2.println("ecranOLED_2");
+
+  ecranOLED_2.display();                            // Transfer buffer to screen
   delay(2000);
 //  int16_t posX = 40;
 //  int16_t posY = 20;
