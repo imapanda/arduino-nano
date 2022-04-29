6d4e8737a120212258192d0c8c1f9ffebbbc0773
Clément BRESSY <clement.bressy@protonmail.com>
2022-04-29 23:57:22 +0200

    Format code + comments

--- arduino-nano_7-segments-dht11/arduino-nano_7-segments-dht11.ino
+++ arduino-nano_7-segments-dht11/arduino-nano_7-segments-dht11.ino
@@ -15,16 +15,16 @@
 #include <dht.h>
 #include "Ticker.h" // See https://github.com/sstaub/Ticker for more infos
 
+
+//------------------
+// CONST VARIABLES
+//------------------
+
 #define DEBUG 0 //Quick debug for module 1 (multiplexing 7 segment cathodes)
 
-#if DEBUG
-const unsigned int DISPLAY_TICKS = 50;  // 25 images secondes = 40ms = 1/25; 100hz = 10ms
-#else
-const unsigned int DISPLAY_TICKS = 10;  // 25 images secondes = 40ms = 1/25; 100hz = 10ms
-#endif
-
-const unsigned int DHT_11_READ_TIMING = 10000;  // Delay between each DHT11 reads (ms)
-const unsigned int DISPLAY_MODE_INTERVAL = 2000;  // Change display screen timer (ms)
+const unsigned int DHT11_PIN       = 4; // For dht.h
+const unsigned int DHT11_LED_PIN   = 3; // For dht11 led on ticker
+const unsigned int DISPLAY_LED_PIN = 2; // Lights up led when reading sensor
 
 // GPIO Pinout for multiplexing displays, this is done with hardware logic gates.
 // This allows us to save gpio pins for other uses.
@@ -32,6 +32,21 @@ const unsigned int MULTIPLEXING_PIN = 5;
 
 // GPIO Pinout for 7 segment display
 const unsigned int SEGMENT_PINS[] = {12, 11, 10, 9, 8, 7, 6, 5 };   // { a b c d e f g . )
+
+
+//------------
+// CONSTANTS
+//------------
+
+#if DEBUG
+  const unsigned int DISPLAY_TICKS = 100;  // 25 images secondes = 40ms = 1/25; 100hz = 10ms
+#else
+  const unsigned int DISPLAY_TICKS = 2;  // 25 images secondes = 40ms = 1/25; 100hz = 10ms
+#endif
+
+const unsigned int DHT_11_READ_TIMING = 10000;  // Delay between each DHT11 reads (ms)
+const unsigned int DISPLAY_MODE_INTERVAL = 2000;  // Change display screen timer (ms)
+
 const byte segCode[11][8] = {
   //  a  b  c  d  e  f  g  .
   { 1, 1, 1, 1, 1, 1, 0, 0},  // 0
@@ -47,20 +62,26 @@ const byte segCode[11][8] = {
   { 0, 0, 0, 0, 0, 0, 0, 1}   // .
 };
 
+
+//-----------------------
+// Functions signatures
+//-----------------------
+
 void update_dht11_values(void);
 void update_counter_value(void);
 void update_display(void);
 void displayTemp(void);
 void displayHumidity(void);
 
-// DHT11 settings
-const unsigned int DHT11_PIN = 4;       // For dht.h
-const unsigned int DHT11_LED_PIN = 3;   // For dht11 led on ticker
-const unsigned int DISPLAY_LED_PIN = 2;
 
-Ticker dht11_updater   (update_dht11_values, DHT_11_READ_TIMING);       //calls function update_dht11_values() every DHT_11_READ_TIMING seconds, internal resolution is micros, running endless
-Ticker counter_updater (update_counter_value, DISPLAY_MODE_INTERVAL);   // Change display screen timer
-Ticker display_update (update_display, DISPLAY_TICKS);                  //25 images secondes = 40ms = 1/25; 100hz = 10ms
+//-------------------
+// Global variables
+//-------------------
+
+// All these tickers are running endless
+Ticker dht11_updater   (update_dht11_values,   DHT_11_READ_TIMING);      //calls function update_dht11_values() every DHT_11_READ_TIMING seconds, internal resolution is micros
+Ticker counter_updater (update_counter_value,  DISPLAY_MODE_INTERVAL);   // Change display screen timer
+Ticker display_update  (update_display,        DISPLAY_TICKS);           //25 images secondes = 40ms = 1/25; 100hz = 10ms
 
 dht DHT;
 float temp = 0;
@@ -76,13 +97,17 @@ unsigned int counter = 0;
 unsigned int temperature_value = 0;
 unsigned int humidity_value = 0;
 
+// global array of pointers to functions
+void (*functions[2])(void) = {displayTemp, displayHumidity};  
 
-void (*functions[2])(void) = {displayTemp, displayHumidity};  // Array of pointers to functions
 
+//------------
+// Functions
+//------------
 
 void displayDigit(int digit) {
   // Write values of digit using segCode onto SEGMENT_PINS
-  int i;
+  uint8_t i;
   for (i = 0; i < 8; i++) {
     digitalWrite(SEGMENT_PINS[i], segCode[digit][i]);
   }
@@ -91,14 +116,8 @@ void displayDigit(int digit) {
 
 
 void update_counter_value(void) {
-  //  counter++;
-  //  if (counter > 1) {
-  //    counter = 0;
-  //  }
-  //  Serial.println(counter);
-  //
-  // trick 1 "little player" :
-  counter = 1 - counter;  // alternate between 0 and 1 values
+  // little trick alternate between 0 and 1 values :
+  counter = 1 - counter;
   return;
 }
 
@@ -108,14 +127,13 @@ void update_dht11_values(void) {
   //Light up temp reading led
   digitalWrite(DHT11_LED_PIN, HIGH);
 
-  // Display "-" on display (digit one) while reading :
-  int i;
+  // Display "-" on digit n°1 while reading :
+  uint8_t i;
   for (i = 0; i < 8; i++) { // clearing
     digitalWrite(SEGMENT_PINS[i], 0);
   }
   digitalWrite(6, HIGH);
   
-
   DHT.read11(DHT11_PIN);
   Serial.print("Current humidity = ");
   Serial.print(DHT.humidity);
@@ -134,6 +152,7 @@ void update_dht11_values(void) {
   return;
 }
 
+
 void displayTemp(void) {
 
   digitalWrite(DISPLAY_LED_PIN, HIGH);
@@ -149,6 +168,7 @@ void displayTemp(void) {
 
   return;
 }
+
 
 void displayHumidity(void) {
 
@@ -169,33 +189,41 @@ void displayHumidity(void) {
 
 // the setup function runs once when you press reset or power the board
 void setup() {
+  
 
   Serial.begin(9600);
-
+  
   // initialize digital pins as outputs.
-  for (int i = 0; i < 8; i++) {
+  uint8_t i;
+  for (i = 0; i < 8; i++) {
     pinMode(SEGMENT_PINS[i], OUTPUT);
   }
-
   pinMode(MULTIPLEXING_PIN, OUTPUT);
 
   pinMode(DHT11_PIN, INPUT);
   pinMode(DHT11_LED_PIN, OUTPUT);
   pinMode(DISPLAY_LED_PIN, OUTPUT);
 
-  delay(1234); // Simulates boots time
-
   dht11_updater.start();
   counter_updater.start();
   display_update.start();
   
+  // blink 10 times for setup OK
+  digitalWrite(DHT11_LED_PIN, LOW);
+  for (i = 0; i < 10; i++) {
+    digitalWrite(DHT11_LED_PIN, HIGH);
+    delay(100);
+    digitalWrite(DHT11_LED_PIN, LOW);
+    delay(100);
+  }
   return;
 }
 
 
 void update_display(void) {
   functions[counter & 1]();  // switch between each of the 2 functions of the array. Easy to add/remove functions from the array.
-  
+  Serial.print("switching display mode");
+  Serial.print(counter & 1);
   return;
 }
 
